0df281dd6536a7303b6f4333ad61d6cd36e3a94b
Bobert2337 <milanrobert222@gmail.com>
2024-08-27 15:04:41 +0300

    Am adaugat comentariu

--- p178.cpp
+++ p178.cpp
@@ -1,4 +1,7 @@
 //Să se scrie un program care verifică dacă un număr natural citit de la tastatură este pătrat perfect.
+//Programul citește de la tastatură numărul natural n.
+//Programul afișează pe ecran mesajul da, dacă n este pătrat perfect, respectiv nu în caz contrar.
+//n < 2.000.000.000
 #include <iostream>
 #include <cmath>
 using namespace std;
